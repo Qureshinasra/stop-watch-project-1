5e12ebbf23f91dd227b94d4a3eedd3cf69ab46d3
Qureshinasra <qureshinasra47@gmail.com>
2024-08-19 13:33:23 +0530

    msg

--- sript.js
+++ sript.js
@@ -1,78 +1,40 @@
-let startBtn = document.getElementById('start'); 
-let stopBtn = document.getElementById('stop'); 
-let resetBtn = document.getElementById('reset'); 
+// Javascript code
+var startTime, elapsedTime, timerInterval;
 
-let hour = 00; 
-let minute = 00; 
-let second = 00; 
-let count = 00; 
-
-startBtn.addEventListener('click', function () { 
-	timer = true; 
-	stopWatch(); 
-}); 
-
-stopBtn.addEventListener('click', function () { 
-	timer = false; 
-}); 
-
-resetBtn.addEventListener('click', function () { 
-	timer = false; 
-	hour = 0; 
-	minute = 0; 
-	second = 0; 
-	count = 0; 
-	document.getElementById('hr').innerHTML = "00"; 
-	document.getElementById('min').innerHTML = "00"; 
-	document.getElementById('sec').innerHTML = "00"; 
-	document.getElementById('count').innerHTML = "00"; 
-}); 
-
-function stopWatch() { 
-	if (timer) { 
-		count++; 
-
-		if (count == 100) { 
-			second++; 
-			count = 0; 
-		} 
-
-		if (second == 60) { 
-			minute++; 
-			second = 0; 
-		} 
-
-		if (minute == 60) { 
-			hour++; 
-			minute = 0; 
-			second = 0; 
-		} 
-
-		let hrString = hour; 
-		let minString = minute; 
-		let secString = second; 
-		let countString = count; 
-
-		if (hour < 10) { 
-			hrString = "0" + hrString; 
-		} 
-
-		if (minute < 10) { 
-			minString = "0" + minString; 
-		} 
-
-		if (second < 10) { 
-			secString = "0" + secString; 
-		} 
-
-		if (count < 10) { 
-			countString = "0" + countString; 
-		} 
-
-		document.getElementById('hr').innerHTML = hrString; 
-		document.getElementById('min').innerHTML = minString; 
-		document.getElementById('sec').innerHTML = secString; 
-		document.getElementById('count').innerHTML = countString; 
-		setTimeout(stopWatch, 10); 
-	} 
+function startTimer() {
+  startTime = Date.now() - elapsedTime;
+  timerInterval = setInterval(function () {
+    var currentTime = Date.now();
+    elapsedTime = currentTime - startTime;
+    updateDisplay(elapsedTime);
+  }, 10);
 }
+
+function stopTimer() {
+  clearInterval(timerInterval);
+}
+
+function resetTimer() {
+  clearInterval(timerInterval);
+  elapsedTime = 0;
+  updateDisplay(elapsedTime);
+}
+
+function updateDisplay(time) {
+  var milliseconds = Math.floor((time % 1000) / 10);
+  var seconds = Math.floor((time / 1000) % 60);
+  var minutes = Math.floor((time / 1000 / 60) % 60);
+  var hours = Math.floor((time / 1000 / 60 / 60) % 24);
+
+  var displayTime = hours.toString().padStart(2, '0') + ':' +
+                    minutes.toString().padStart(2, '0') + ':' +
+                    seconds.toString().padStart(2, '0') + '.' +
+                    milliseconds.toString().padStart(2, '0');
+
+  document.querySelector('.display').textContent = displayTime;
+}
+
+document.querySelector('.start').addEventListener('click', startTimer);
+document.querySelector('.stop').addEventListener('click', stopTimer);
+document.querySelector('.reset').addEventListener('click', resetTimer);
+
