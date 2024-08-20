2b4a9ba0974e60ea724454c23d0a9e93eb0a15dd
unknown <qureshinasra47@gmail.com>
2024-08-20 17:04:47 +0530

    msg

--- sript.js
+++ sript.js
@@ -1,40 +1,68 @@
-// Javascript code
-var startTime, elapsedTime, timerInterval;
+let startButton = document.getElementById('start');
+let stopButton = document.getElementById('stop');
+let resetButton = document.getElementById('reset');
 
-function startTimer() {
-  startTime = Date.now() - elapsedTime;
-  timerInterval = setInterval(function () {
-    var currentTime = Date.now();
-    elapsedTime = currentTime - startTime;
-    updateDisplay(elapsedTime);
-  }, 10);
+let hour = 00;
+let minute = 00;
+let second = 00;
+let count = 00;
+
+startButton.addEventListener('click', function () {
+    timer = true;
+    stopWatch();
+});
+stopButton.addEventListener('click', function () {
+    timer = false;
+});
+resetButton.addEventListener('click', function () {
+    timer = false;
+    hour = 0;
+    minute = 0;
+    second = 0;
+    count = 0;
+    document.getElementById('hour').innerHTML = "00";
+    document.getElementById('minute').innerHTML = "00";
+    document.getElementById('second').innerHTML = "00";
+    document.getElementById('count').innerHTML = "00";
+});
+
+function stopWatch() {
+    if (timer) {
+        count++;
+        if (count == 100) {
+            second++;
+            count = 0;
+        }
+        if (second == 60) {
+            minute++;
+            second = 0;
+        }
+        if (minute == 60) {
+            hour++;
+            minute = 0;
+            second = 0;
+        }
+        let hourString = hour;
+        let minuteString = minute;
+        let secondString = second;
+        let countString = count;
+        if (hour < 10) {
+            hourString = "0" + hourString;
+        }
+        if (minute < 10) {
+            minuteString = "0" + minuteString;
+        }
+        if (second < 10) {
+            secondString = "0" + secondString;
+        }
+
+        if (count < 10) {
+            countString = "0" + countString;
+        }
+        document.getElementById('hour').innerHTML = hourString;
+        document.getElementById('minute').innerHTML = minuteString;
+        document.getElementById('second').innerHTML = secondString;
+        document.getElementById('count').innerHTML = countString;
+        setTimeout(stopWatch, 10);
+    }
 }
-
-function stopTimer() {
-  clearInterval(timerInterval);
-}
-
-function resetTimer() {
-  clearInterval(timerInterval);
-  elapsedTime = 0;
-  updateDisplay(elapsedTime);
-}
-
-function updateDisplay(time) {
-  var milliseconds = Math.floor((time % 1000) / 10);
-  var seconds = Math.floor((time / 1000) % 60);
-  var minutes = Math.floor((time / 1000 / 60) % 60);
-  var hours = Math.floor((time / 1000 / 60 / 60) % 24);
-
-  var displayTime = hours.toString().padStart(2, '0') + ':' +
-                    minutes.toString().padStart(2, '0') + ':' +
-                    seconds.toString().padStart(2, '0') + '.' +
-                    milliseconds.toString().padStart(2, '0');
-
-  document.querySelector('.display').textContent = displayTime;
-}
-
-document.querySelector('.start').addEventListener('click', startTimer);
-document.querySelector('.stop').addEventListener('click', stopTimer);
-document.querySelector('.reset').addEventListener('click', resetTimer);
-
